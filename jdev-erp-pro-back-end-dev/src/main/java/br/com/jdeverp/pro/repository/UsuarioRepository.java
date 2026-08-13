4113c81268643bdfbbea06771cba3f6b9f3d26d9
Francivaldo Costa <francivaldoaccosta@gmail.com>
2026-08-13 01:55:30 -0300

    Terminando módulo 27

---
+++ jdev-erp-pro-back-end-dev/src/main/java/br/com/jdeverp/pro/repository/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package br.com.jdeverp.pro.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.jdeverp.pro.anotacoes.IgnoreEmpresaId;
+import br.com.jdeverp.pro.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaJdevRepository<Usuario, Long> {
+	
+	
+	@IgnoreEmpresaId(ignorar = true, motivo = "Usado para login, então a empresa é estabelecida depois do login do usuário")
+	@Query("select u from Usuario u where u.login = :login")
+	Usuario buscaPorLogin(@Param("Login") String Login);
+	
+
+}
--- jdev-erp-pro-back-end-dev/src/main/java/br/com/jdeverp/pro/validator/RepositoryEmpresaValidator.java
+++ jdev-erp-pro-back-end-dev/src/main/java/br/com/jdeverp/pro/validator/RepositoryEmpresaValidator.java
@@ -1,46 +1,108 @@
 package br.com.jdeverp.pro.validator;
 
-import java.lang.ProcessHandle.Info;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.core.support.RepositoryFactoryInformation;
 import org.springframework.stereotype.Component;
 
+import br.com.jdeverp.pro.anotacoes.IgnoreEmpresaId;
+
 @Component
-public class RepositoryEmpresaValidator implements SmartInitializingSingleton{
-	
-	
+public class RepositoryEmpresaValidator implements SmartInitializingSingleton {
+
 	private final ApplicationContext applicationContext;
-	
+
 	public RepositoryEmpresaValidator(ApplicationContext applicationContext) {
 		this.applicationContext = applicationContext;
 	}
-	
-	
-	
+
 	private List<Class<?>> getRepositories() {
 		List<Class<?>> repositores = new ArrayList<Class<?>>();
-		applicationContext.getBeansOfType(RepositoryFactoryInformation.class)
-						.values().forEach(Info -> repositores.add(Info.getRepositoryInformation().getRepositoryInterface()));
-		
+		applicationContext.getBeansOfType(RepositoryFactoryInformation.class).values()
+				.forEach(Info -> repositores.add(Info.getRepositoryInformation().getRepositoryInterface()));
+
 		return repositores;
-		
-//		PAREI_AQUI
-		
+
 	}
-	
-	
 
 	@Override
 	public void afterSingletonsInstantiated() {
-
-	
 		
+		for (Class<?> interfaceRepository: getRepositories()) {
+
+			/* Ignora o repository inteiro */
+			if (interfaceRepository.isAnnotationPresent(IgnoreEmpresaId.class)) {
+				continue;
+			}
+			
+			for (Method method: interfaceRepository.getMethods()) {
+				
+				/* Ignora apenas esse método que contém @IgnoreEmpresaId */
+				if (method.isAnnotationPresent(IgnoreEmpresaId.class)) {
+					continue;
+				}
+				
+				/* Ignora se for método da extensão */
+				if (!method.getDeclaringClass().equals(interfaceRepository)) {
+					continue;
+				}
+				
+				/* Verificar se tem @Query escrito no método */
+				boolean queryPresent = method.isAnnotationPresent(Query.class);
+				
+				if (!queryPresent) {
+					throw new IllegalStateException("O método: " + method + " da interface: " + interfaceRepository + " deve possuir Query escrita.");
+				}
+				
+				Query query = method.getAnnotation(Query.class);
+				
+				String sql = query.value().toLowerCase();
+				
+				/* Continua se tiver empresaId e está correto */
+				if (sql.contains("empresa.id") || sql.contains("empresa_id")) {
+					continue;
+				}
+				
+				throw new IllegalStateException("""
+
+						====================================================================
+						ERRO DE SEGURANÇA
+
+						Repository.....: %s
+						Método.........: %s
+
+						A consulta abaixo NÃO possui filtro por empresa.
+
+						%s
+
+						Toda consulta deve possuir:
+
+						empresa.id
+
+						ou
+
+						empresa_id
+
+						Caso esta consulta realmente não necessite
+						do filtro, utilize:
+
+						@IgnoreEmpresaId
+
+						Essa anotação pode ser usada para o Repository completo ou para métodos unicos.
+						====================================================================
+
+						""".formatted(interfaceRepository.getSimpleName(), method.getName(), query.value()));
+				
+			}
+			
+		}
+		
+
 	}
-	
-	
 
 }
